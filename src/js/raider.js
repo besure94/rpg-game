b032bab1dab629c1f08123a0cb5ae735871f45c2
Brian Scherner <brianscherner@gmail.com>
2023-08-08 20:56:22 -0700

    Write methods for assignName and assignNumbers.

--- src/js/raider.js
+++ src/js/raider.js
@@ -5,5 +5,18 @@ export class Raider {
     this.stealth = 0;
     this.accuracy = 0;
     this.speech = 0;
+    this.name = "";
+  }
+
+  assignName(playerName) {
+    this.name = playerName;
+  }
+
+  assignNumbers(speedAttr, strengthAttr, stealthAttr, accuracyAttr, speechAttr) {
+    this.speed = speedAttr;
+    this.strength = strengthAttr;
+    this.stealth = stealthAttr;
+    this.accuracy = accuracyAttr;
+    this.speech = speechAttr;
   }
 }
