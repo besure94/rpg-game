e6c6e563d38961aa6da110d7e1c7652401518631
Brian Scherner <brianscherner@gmail.com>
2023-08-10 18:56:25 -0700

    Write constructor and methods for class.

---
+++ src/js/scout.js
@@ -0,0 +1,22 @@
+export class Scout {
+  constructor() {
+    this.speed = 0;
+    this.strength = 0;
+    this.stealth = 0;
+    this.accuracy = 0;
+    this.speech = 0;
+    this.name = "";
+  }
+
+  assignName(playerName) {
+    this.name = playerName;
+  }
+
+  assignNumbers(speedAttr, strengthAttr, stealthAttr, accuracyAttr, speechAttr) {
+    this.speed = speedAttr;
+    this.strength = strengthAttr;
+    this.stealth = stealthAttr;
+    this.accuracy = accuracyAttr;
+    this.speech = speechAttr;
+  }
+}
