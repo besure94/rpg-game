bb2dc98c8cabbc550761fc2aa03c5d1871defd65
Brian Scherner <brianscherner@gmail.com>
2023-08-10 18:55:43 -0700

    Write passing tests for properties, name and attributes.

---
+++ __tests__/scout.test.js
@@ -0,0 +1,29 @@
+import { Scout } from '../src/js/scout.js';
+
+describe('Scout', () => {
+  let scout;
+
+  beforeEach(() => {
+    scout = new Scout();
+  });
+
+  test('should create a scout object with five properties, with each property being a 0', () => {
+    expect(scout.speed).toEqual(0);
+    expect(scout.strength).toEqual(0);
+    expect(scout.stealth).toEqual(0);
+    expect(scout.accuracy).toEqual(0);
+    expect(scout.speech).toEqual(0);
+  });
+  test('should contain a method to create a name that the user can enter', () => {
+    scout.assignName("Slane");
+    expect(scout.name).toEqual("Slane");
+  });
+  test('should contain a method that will add a number between 1 and 5 to each property', () => {
+    scout.assignNumbers(5, 2, 5, 2, 4);
+    expect(scout.speed).toEqual(5);
+    expect(scout.strength).toEqual(2);
+    expect(scout.stealth).toEqual(5);
+    expect(scout.accuracy).toEqual(2);
+    expect(scout.speech).toEqual(4);
+  });
+});
