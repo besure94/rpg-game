b87b62d41d9f08d62b81f61cb6bd42ff71d3728e
Brian Scherner <brianscherner@gmail.com>
2023-08-13 18:47:54 -0700

    Add five new properties to class and assignNumbers method.

--- src/js/soldier.js
+++ src/js/soldier.js
@@ -5,6 +5,11 @@ export class Soldier {
     this.stealth = 0;
     this.accuracy = 0;
     this.speech = 0;
+    this.heavyWeapons = 0;
+    this.lightWeapons = 0;
+    this.intelligence = 0;
+    this.melee = 0;
+    this.healing = 0;
     this.name = "";
   }
 
@@ -12,11 +17,16 @@ export class Soldier {
     this.name = playerName;
   }
 
-  assignNumbers(speedAttr, strengthAttr, stealthAttr, accuracyAttr, speechAttr) {
+  assignNumbers(speedAttr, strengthAttr, stealthAttr, accuracyAttr, speechAttr, heavyAttr, lightAttr, intelAttr, meleeAttr, healingAttr) {
     this.speed = speedAttr;
     this.strength = strengthAttr;
     this.stealth = stealthAttr;
     this.accuracy = accuracyAttr;
     this.speech = speechAttr;
+    this.heavyWeapons = heavyAttr;
+    this.lightWeapons = lightAttr;
+    this.intelligence = intelAttr;
+    this.melee = meleeAttr;
+    this.healing = healingAttr;
   }
 }
