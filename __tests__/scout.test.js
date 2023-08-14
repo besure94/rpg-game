838d4fd05b8c7fe087017792b6ccf59673cff54d
Brian Scherner <brianscherner@gmail.com>
2023-08-13 18:40:50 -0700

    Update tests to include five new properties.

--- __tests__/scout.test.js
+++ __tests__/scout.test.js
@@ -7,23 +7,33 @@ describe('Scout', () => {
     scout = new Scout();
   });
 
-  test('should create a scout object with five properties, with each property being a 0', () => {
+  test('should create a scout object with ten properties, with each property being a 0', () => {
     expect(scout.speed).toEqual(0);
     expect(scout.strength).toEqual(0);
     expect(scout.stealth).toEqual(0);
     expect(scout.accuracy).toEqual(0);
     expect(scout.speech).toEqual(0);
+    expect(scout.heavyWeapons).toEqual(0);
+    expect(scout.lightWeapons).toEqual(0);
+    expect(scout.intelligence).toEqual(0);
+    expect(scout.melee).toEqual(0);
+    expect(scout.healing).toEqual(0);
   });
   test('should contain a method to create a name that the user can enter', () => {
     scout.assignName("Slane");
     expect(scout.name).toEqual("Slane");
   });
-  test('should contain a method that will add a number between 1 and 5 to each property', () => {
-    scout.assignNumbers(5, 2, 5, 2, 4);
+  test('should contain a method that will assign a number between 1 and 5 to each property', () => {
+    scout.assignNumbers(5, 2, 5, 2, 4, 1, 2, 5, 3, 4);
     expect(scout.speed).toEqual(5);
     expect(scout.strength).toEqual(2);
     expect(scout.stealth).toEqual(5);
     expect(scout.accuracy).toEqual(2);
     expect(scout.speech).toEqual(4);
+    expect(scout.heavyWeapons).toEqual(1);
+    expect(scout.lightWeapons).toEqual(2);
+    expect(scout.intelligence).toEqual(5);
+    expect(scout.melee).toEqual(3);
+    expect(scout.healing).toEqual(4);
   });
 });
